1b563e90901d118a757abf82240eddff6ed10bb9
Sri Hari <sriharikvvs@gmail.com>
2024-05-27 10:40:57 +0530

    Add files via upload

---
+++ alpha pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+    int alpha;
+    for (alpha=65; alpha<=90;alpha+=1)
+    {
+        printf("%c", alpha);
+    }
+    printf("%c",10);
+    for (alpha=65; alpha<=90;alpha+=1)
+    {
+        printf("~");
+    }
+    return 0;
+}
